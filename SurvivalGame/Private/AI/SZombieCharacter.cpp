963b079eef408d1bf0dc25c127b2f9fd71af65a6
Son Dong Kyu <fkffksk20@gmail.com>
2020-05-07 14:53:28 +0900

    -1 f

--- SurvivalGame/Private/AI/SZombieCharacter.cpp
+++ SurvivalGame/Private/AI/SZombieCharacter.cpp
@@ -15,23 +15,19 @@
 ASZombieCharacter::ASZombieCharacter(const class FObjectInitializer& ObjectInitializer)
 : Super(ObjectInitializer)
 {
-	/* Note: We assign the Controller class in the Blueprint extension of this class 
-		Because the zombie AIController is a blueprint in content and it's better to avoid content references in code.  */
-	/*AIControllerClass = ASZombieAIController::StaticClass();*/
 
-	/* Our sensing component to detect players by visibility and noise checks. */
 	PawnSensingComp = CreateDefaultSubobject<UPawnSensingComponent>(TEXT("PawnSensingComp"));
 	PawnSensingComp->SetPeripheralVisionAngle(60.0f);
 	PawnSensingComp->SightRadius = 2000;
 	PawnSensingComp->HearingThreshold = 600;
 	PawnSensingComp->LOSHearingThreshold = 1200;
 
-	/* Ignore this channel or it will absorb the trace impacts instead of the skeletal mesh */
+	
 	GetCapsuleComponent()->SetCollisionResponseToChannel(COLLISION_WEAPON, ECR_Ignore);
 	GetCapsuleComponent()->SetCapsuleHalfHeight(96.0f, false);
 	GetCapsuleComponent()->SetCapsuleRadius(42.0f);
 
-	/* These values are matched up to the CapsuleComponent above and are used to find navigation paths */
+	
 	GetMovementComponent()->NavAgentProps.AgentRadius = 42;
 	GetMovementComponent()->NavAgentProps.AgentHeight = 192;
 
@@ -55,9 +51,8 @@ ASZombieCharacter::ASZombieCharacter(const class FObjectInitializer& ObjectIniti
 
 	/* By default we will not let the AI patrol, we can override this value per-instance. */
 	BotType = EBotBehaviorType::Passive;
-	SenseTimeOut = 2.5f;
+	SenseTimeOut = 1.5f;
 
-	/* Note: Visual Setup is done in the AI/ZombieCharacter Blueprint file */
 }
 
 
