0bee09696cf42bbce9728f14426ca4c66efcd76c
Son Dong Kyu <fkffksk20@gmail.com>
2020-05-10 21:15:27 +0900

    2

--- SurvivalGame/Private/World/SGameState.cpp
+++ SurvivalGame/Private/World/SGameState.cpp
@@ -9,7 +9,7 @@ ASGameState::ASGameState(const class FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
 {
 	/* 1 SECOND real time is 1*TimeScale MINUTES game time */
-	TimeScale = 17.0f;
+	TimeScale = 16.0f;
 	bIsNight = false;
 
 	SunriseTimeMark = 3.0f;
